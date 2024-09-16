db83f2a849d70170839a6ed16d4ec97573eb09d8
Pozdniakov <bucherr@yandex.ru>
2024-09-16 11:54:39 +0200

    add first script

---
+++ 01_intro_to_R.R
@@ -0,0 +1,105 @@
+2 + 2 - 1
+5 * 6 #multiplication
+#it is division 
+
+2 - 1
+3 + 5
+
+365 * 24 * 60 * 60
+
+121 / 11
+12 ^ 2
+
+7 %/% 3
+7 %% 3
+
+16 ^ 0.5
+SQRT(16)
+sqrt(16)
+log(8)
+?log
+log(x = 8, base = 2)
+log(8, 2)
+log(100, 5 + 5)
+
+`+`(5, 5)
+(2 + 2) * 2
+
+pi
+letters
+LETTERS
+month.name
+month.abb
+
+sin(pi)
+1/3
+options(scipen = 7)
+sin(pi)
+2 ^ 100
+
+options(scipen = 999)
+2 ^ 100
+
+a = log(8, 2)
+a <- log(8,2)
+b <- 4
+# 4 -> b #we don't do it here
+
+a
+a + b
+
+e <- 42
+rm(e)
+rm(list = ls())
+
+a <- log(8,2)
+b <- 4
+
+a == b
+a == a
+
+a != b
+a != a
+
+a > b
+a < b
+a >= b
+b >= b
+a <= b
+a <= a
+#3 + 2i
+
+is.integer(5)
+is.double(5)
+is.integer(5L)
+5L
+as.integer(5)
+as.double(5L)
+as.complex(5)
+s <- "hi everyone"
+s
+s <- 'hi, "everyone" '
+s
+
+paste("hey", "hey", "ho", sep = "_<3___")
+paste("hey", "hey", "ho", sep = "")
+paste0("hey", "hey", "ho")
+?paste
+
+comparison <- a < b
+t1 <- TRUE
+f1 <- FALSE
+
+!t1
+!f1
+!!t1
+
+t1 & t1 #true
+t1 & f1 #false
+f1 & t1 #false
+f1 & f1 #false
+
+f1 | f1 #false
+t1 | f1 #true
+f1 | t1 #true
+t1 | t1 #false
